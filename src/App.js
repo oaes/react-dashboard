cb0f2c80a955abf8847443905e058dd82f16f4e9
Oaes Kuruni <oaeskuruni27@gmail.com>
2020-04-02 13:40:42 +0600

    working on it

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './Components/Signin-side/Signin-side';
+function App() {
+  return (
+    <div className="App">
+      <Sidebar></Sidebar>
+    </div>
+  );
+}
+
+export default App;
